00094eddc5159b47ca673a571605d099ba65da0e
Andrew Bonner <andrewbonner@Andrews-Air.Home>
2026-02-08 15:29:50 +0000

    Add instructions to seed column migration

--- add_seed_column.sql
+++ add_seed_column.sql
@@ -1,2 +1,4 @@
+
 -- Run this in your Supabase SQL Editor to add the 'seed' column to the players table
+-- This is required for the strict scheduler to work with seeded fairness
 ALTER TABLE public.players ADD COLUMN seed INTEGER;
